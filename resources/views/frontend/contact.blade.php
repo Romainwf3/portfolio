a72de32e523d280e2e52b7762077306dfa46f48c
Romainwf3 <romainwf3@gmail.com>
2019-10-27 14:44:38 +0100

    nav

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,101 +1,64 @@
 @extends('frontend.layouts.app')
-
-@section('title', app_name() . ' | ' . __('labels.frontend.contact.box_title'))
-
-@section('content')
-    <div class="row justify-content-center">
-        <div class="col col-sm-8 align-self-center">
-            <div class="card">
-                <div class="card-header">
-                    <strong>
-                        @lang('labels.frontend.contact.box_title')
-                    </strong>
-                </div><!--card-header-->
-
-                <div class="card-body">
-                    {{ html()->form('POST', route('frontend.contact.send'))->open() }}
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.name'))->for('name') }}
-
-                                    {{ html()->text('name', optional(auth()->user())->name)
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.name'))
-                                        ->attribute('maxlength', 191)
-                                        ->required()
-                                        ->autofocus() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
-
-                                    {{ html()->email('email', optional(auth()->user())->email)
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.email'))
-                                        ->attribute('maxlength', 191)
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.phone'))->for('phone') }}
-
-                                    {{ html()->text('phone')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.phone'))
-                                        ->attribute('maxlength', 191)
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.message'))->for('message') }}
-
-                                    {{ html()->textarea('message')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.message'))
-                                        ->attribute('rows', 3)
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        @if(config('access.captcha.contact'))
-                            <div class="row">
-                                <div class="col">
-                                    @captcha
-                                    {{ html()->hidden('captcha_status', 'true') }}
-                                </div><!--col-->
-                            </div><!--row-->
-                        @endif
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group mb-0 clearfix">
-                                    {{ form_submit(__('labels.frontend.contact.button')) }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-                    {{ html()->form()->close() }}
-                </div><!--card-body-->
-            </div><!--card-->
-        </div><!--col-->
-    </div><!--row-->
+@section('style')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/index.css') }}" />
 @endsection
 
-@push('after-scripts')
-    @if(config('access.captcha.contact'))
-        @captchaScripts
-    @endif
-@endpush
+@section('title', app_name() . ' | ' . __('navs.general.home'))
+@section('content')
+
+    <header id="header001">
+        <div class="navbar fixed-top bg-transparent  navInv">
+            <button class="navbar-toggler outline" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <img class="imgtest bordure" src="{{ asset ('../img/frontend/profil.jpg')}}" alt="">
+            </button>
+            <div class="bordure collapse navbar-collapse p-0" id="navbarTogglerDemo01">
+
+                <ul class="nav nav-burger-link bg-white navbar-nav mr-auto d-flex justify-content-between">
+                    <li class="nav-item d-sm-none d-lx-block mt-2">
+                    <a class="nav-link" href="#">Acceuil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item mt-2">
+                    <a class="nav-link mt-1" href="#presentation">Pésentation</a>
+                    </li>
+                    <li class="nav-item mt-2">
+                    <a class="nav-link mt-1" href="#site">Mes Projets</a>
+                    </li>
+                    <li class="nav-item mt-2">
+                    <a class="nav-link mt-1" href="#footer001">Contact</a>
+                    </li>
+                    <li class="nav-item d-none d-sm-block mt-2">
+                        <div class="d-flex justify-content-center p-0">
+                            <div class="linkedin mt-1">Linkedin :</div>
+                            <a class="iconLinkedin nav-link mb-2" href="https://www.linkedin.com/in/romain-verwaerde/" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
+                        </div>
+                    </li>
+                    <li class="nav-item d-block d-sm-none mt-2">
+                        <a class="iconLinkedin nav-link" href="https://www.linkedin.com/in/romain-verwaerde/" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="inv"></div>
+        <div class="head1">
+            <p class="textHeader">Romain Verwaerde</p>
+            <p class="textHeader">Dévellopeur web</p>
+        </div>
+    </header>
+    <aside>
+        <nav class="bordure">
+            <a class="lienNav te" href="#header001">
+                <img class="imgProfil" src="{{ asset ('../img/frontend/profil.jpg')}}" alt="">
+            </a>
+
+            <a class="lienNav" href="#presentation">A propos</a>
+
+            <a class="lienNav" href="#site">Mes Projets</a>
+
+            <a class="lienNav" href="#footer001">Contact</a>
+            @include('includes.partials.messages')
+
+            <h4>Linkedin :</h4>
+            <a class="iconLinkedin lienNav" href="https://www.linkedin.com/in/romain-verwaerde/" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
+        </nav>
+    </aside>
+@endsection
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -7,34 +7,34 @@
 @section('content')
 
     <header id="header001">
-        <div class="navbar  fixed-top bg-transparent navbar-expand-sm navInv">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <img class="imgtest" src="{{ asset ('../img/frontend/profil.jpg')}}" alt="">
+        <div class="navbar fixed-top bg-transparent navbar-expand-sm navInv">
+            <button class="navbar-toggler butP p-2" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <img class="imgtest bordure" src="{{ asset ('../img/frontend/profil.jpg')}}" alt="">
             </button>
-            <div class="collapse navbar-collapse p-0" id="navbarTogglerDemo01">
+            <div class="bordure collapse navbar-collapse p-0" id="navbarTogglerDemo01">
                 <a class="nav-link te d-none d-sm-block te2" href="#">
                     <img class="imgtest " src="{{ asset ('../img/frontend/profil.jpg')}}" alt="">
                 </a>
-                <ul class="nav nav-burger-link bg-white navbar-nav mt-2 mr-auto d-flex justify-content-between">
+                <ul class="nav nav-burger-link bg-white navbar-nav mr-auto d-flex justify-content-between">
                     <li class="nav-item d-sm-none d-lx-block">
                     <a class="nav-link" href="#">Acceuil <span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mt-2">
                     <a class="nav-link mt-1" href="#presentation">Pésentation</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mt-2">
                     <a class="nav-link mt-1" href="#site">Mes Projets</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mt-2">
                     <a class="nav-link mt-1" href="#footer001">Contact</a>
                     </li>
-                    <li class="nav-item d-none d-sm-block">
+                    <li class="nav-item d-none d-sm-block mt-2">
                         <div class="d-flex justify-content-center p-0">
                             <div class="linkedin mt-1">Linkedin :</div>
                             <a class="iconLinkedin nav-link mb-2" href="https://www.linkedin.com/in/romain-verwaerde/" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
                         </div>
                     </li>
-                    <li class="nav-item d-block d-sm-none">
+                    <li class="nav-item d-block d-sm-none mt-2">
                         <a class="iconLinkedin nav-link" href="https://www.linkedin.com/in/romain-verwaerde/" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
                     </li>
                 </ul>
@@ -47,7 +47,7 @@
         </div>
     </header>
     <aside>
-        <nav>
+        <nav class="bordure">
             <a class="lienNav te" href="#header001">
                 <img class="imgProfil" src="{{ asset ('../img/frontend/profil.jpg')}}" alt="">
             </a>
@@ -66,6 +66,9 @@
 
     <div class="containerMainFooter">
         <main id="jj">
+
+            {{-- <div class="uu">@include('includes.partials.messages')</div> --}}
+
             <section id="presentation" >
                 <h2 id="jjj">Présentation</h2>
                 <div class="divItem">
@@ -96,8 +99,12 @@
 
             <section class="siteItem" id="site">
                 <h2>Sites</h2>
-                <div class="divItem">
+                <div class="divItem divSite">
                     <h3>Zen Et Beauté</h3>
+                    <a href="https://zenetbeaute.dauba.net/" target="_blank"><img class="imgSite" src="{{ asset ('../img/frontend/zenetbeaute.jpg')}}" alt=""></a>
+                </div>
+                <div class="divItem divSite">
+                    <h3>site</h3>
                     <a href="https://zenetbeaute.dauba.net/" target="_blank"><img class="imgSite" src="{{ asset ('../img/frontend/zenetbeaute.jpg')}}" alt=""></a>
                 </div>
             </section>
@@ -107,33 +114,33 @@
                 <h2> Me contacter</h2>
                 <div class="divItem">
                     {{ html()->form('POST', route('frontend.contact.send'))->open() }}
-                        {{ html()->label(__('validation.attributes.frontend.last_name'))->for('last_name') }}
+                        {{-- {{ html()->label(__('validation.attributes.frontend.last_name'))->for('last_name') }} --}}
                         {{ html()->text('last_name', optional(auth()->user())->last_name)
                             ->class('form-control')
                             ->placeholder(__('validation.attributes.frontend.last_name'))
                             ->attribute('maxlength', 191)
                             ->required()
                             }}
-                        {{ html()->label(__('validation.attributes.frontend.first_name'))->for('first_name') }}
+                        {{-- {{ html()->label(__('validation.attributes.frontend.first_name'))->for('first_name') }} --}}
                         {{ html()->text('first_name', optional(auth()->user())->first_name)
                             ->class('form-control')
                             ->placeholder(__('validation.attributes.frontend.first_name'))
                             ->attribute('maxlength', 191)
                             ->required()
                             }}
-                        {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
+                        {{-- {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }} --}}
                         {{ html()->email('email', optional(auth()->user())->email)
                             ->class('form-control')
                             ->placeholder(__('validation.attributes.frontend.email'))
                             ->attribute('maxlength', 191)
                             ->required() }}
-                        {{ html()->label(__('validation.attributes.frontend.subject'))->for('subject') }}
+                        {{-- {{ html()->label(__('validation.attributes.frontend.subject'))->for('subject') }} --}}
                         {{ html()->text('subject')
                             ->class('form-control')
                             ->placeholder(__('validation.attributes.frontend.subject'))
                             ->attribute('maxlength', 191)
                             ->required() }}
-                        {{ html()->label(__('validation.attributes.frontend.message'))->for('message') }}
+                        {{-- {{ html()->label(__('validation.attributes.frontend.message'))->for('message') }} --}}
                         {{ html()->textarea('message')
                             ->class('form-control')
                             ->placeholder(__('validation.attributes.frontend.message'))
@@ -143,7 +150,7 @@
                     {{ html()->form()->close() }}
                     </div>
                     <div class="divItem">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus blanditiis itaque cupiditate doloremque error, fugiat maiores commodi dolores. Repellat voluptas hic harum soluta omnis facilis at incidunt laudantium iure iste!
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus blanditiis itaque cupiditate doloremque error, fugiat maiores commodi dolores. Repellat voluptas hic harum soluta omnis facilis at incidunt laudantium iure iste!</p>
                     </div>
             <section>
         </footer>
